81a51bdb4db7fe028e75255b594848c4bbc2a2e9
Luca Guindani <luca.guindani@gmail.com>
2025-05-01 16:57:31 +0200

    Remove extra empty line

--- lua/lguindani/core/keymaps.lua
+++ lua/lguindani/core/keymaps.lua
@@ -16,4 +16,3 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>t<Right>", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>t<Left>", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
-
