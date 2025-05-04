137c49446df9141e66630be98f97c3a72c2367fd
Luca Guindani <luca.guindani@gmail.com>
2025-05-04 17:37:46 +0200

    Fix codecompanion keys with working ones

--- lua/lguindani/plugins/codecompanion.lua
+++ lua/lguindani/plugins/codecompanion.lua
@@ -7,14 +7,9 @@ return {
       "nvim-treesitter/nvim-treesitter",
     },
     keys = {
-      { "<leader>cc", "<cmd>CodeCompanion<CR>", desc = "Open Code Companion" },
-      { "<leader>cC", "<cmd>CodeCompanionClose<CR>", desc = "Close Code Companion" },
-      { "<leader>cf", "<cmd>CodeCompanionFormat<CR>", desc = "Format Code" },
-      { "<leader>ci", "<cmd>CodeCompanionInfo<CR>", desc = "Show Code Info" },
-      { "<leader>cd", "<cmd>CodeCompanionDebug<CR>", desc = "Debug Code" },
-      { "<leader>ct", "<cmd>CodeCompanionTest<CR>", desc = "Test Code" },
-      { "<leader>cR", "<cmd>CodeCompanionRefactor<CR>", desc = "Refactor Code" },
-      { "<leader>cS", "<cmd>CodeCompanionSearch<CR>", desc = "Search Code" },
+      { "<leader>cci", "<cmd>CodeCompanion<CR>", desc = "Code Companion inline" },
+      { "<leader>ccc", "<cmd>CodeCompanionChat<CR>", desc = "Code Companion chat" },
+      { "<leader>cca", "<cmd>CodeCompanionActions<CR>", desc = "Code Companion actions" },
     },
   }
 }
