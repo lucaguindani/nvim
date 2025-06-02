6bafd51904a10305b8b1ea29427a18c4bdf2188c
Luca Guindani <luca.guindani@gmail.com>
2025-06-02 14:38:48 +0200

    Add nvim-argocd plugin, ignore argocd credentials and update lazy deped

---
+++ lua/lguindani/plugins/argocd.lua
@@ -0,0 +1,8 @@
+return {
+  "lucaguindani/nvim-argocd",
+  branch = "main",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim", -- optional but recommended
+  },
+}
