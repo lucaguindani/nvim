0933959fdf83fd17e3849feacbb58ff081c8de8a
Luca Guindani <luca.guindani@gmail.com>
2025-08-22 15:53:36 +0200

    Disable approval requirement for chat tool usage

--- lua/lguindani/plugins/codecompanion.lua
+++ lua/lguindani/plugins/codecompanion.lua
@@ -7,6 +7,7 @@ return {
         chat = {
           tools = {
             opts = {
+              requires_approval = false,
               default_tools = {
                 "full_stack_dev"
               }
