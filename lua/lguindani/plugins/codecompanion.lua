a70434dfe53756fc084af8b57fb5b27cc5710320
Luca Guindani <luca.guindani@gmail.com>
2026-01-05 13:52:40 +0100

    Update codecompanion to v18 and refactor plugin configuration, update all other dependencies

--- lua/lguindani/plugins/codecompanion.lua
+++ lua/lguindani/plugins/codecompanion.lua
@@ -1,71 +1,59 @@
 return {
   {
     "olimorris/codecompanion.nvim",
-    tag = "v17.33.0",
+    tag = "v18.0.0",
     lazy = false,
     opts = {
-      strategies = {
-        chat = {
-          tools = {
-            opts = {
-              default_tools = {
-                "full_stack_dev"
-              }
-            },
+      tools = {
+        opts = {
+          default_tools = {
+            "full_stack_dev"
           }
-        }
+        },
       },
       adapters = {
-        acp = {
-          opts = {
-            show_defaults = false,
-          },
-          -- Define your custom adapters here
+        opts = {
+          show_defaults = false,
+          show_model_choices = true,
         },
-        http = {
-          opts = {
-            show_defaults = false,
-            show_model_choices = true,
-          },
-          copilot = function()
-            return require("codecompanion.adapters").extend("copilot", {
-              schema = {
-                model = {
-                  default = "gpt-4.1",
-                },
+        copilot = function()
+          return require("codecompanion.adapters").extend("copilot", {
+            schema = {
+              model = {
+                default = "gpt-4.1",
               },
-            })
-          end,
-          ollama = function()
-            return require("codecompanion.adapters").extend("ollama", {
-              name = "ollama",
-              opts = {
-                vision = true,
-                stream = true,
+            },
+          })
+        end,
+        ollama = function()
+          return require("codecompanion.adapters").extend("ollama", {
+            name = "ollama",
+            opts = {
+              vision = true,
+              stream = true,
+            },
+            schema = {
+              model = {
+                default = "qwen3:14b",
               },
-              schema = {
-                model = {
-                  default = "qwen3:14b",
-                },
-                num_ctx = {
-                  default = 16384,
-                },
-                think = {
-                  default = function(adapter)
-                    -- Set `think` to true if the model name contain `qwen3` or `deepseek-r1`
-                    local model_name = adapter.model.name:lower()
-                    return vim.iter({ "qwen3", "deepseek-r1" }):any(function(kw)
-                      return string.find(model_name, kw) ~= nil
-                    end)
-                  end,
-                },
-                keep_alive = {
-                  default = '5m',
-                }
+              num_ctx = {
+                default = 16384,
               },
-            })
-          end,
-        },
+              think = {
+                default = function(adapter)
+                  -- Set `think` to true if the model name contain `qwen3` or `deepseek-r1`
+                  local model_name = adapter.model.name:lower()
+                  return vim.iter({ "qwen3", "deepseek-r1" }):any(function(kw)
+                    return string.find(model_name, kw) ~= nil
+                  end)
+                end,
+              },
+              keep_alive = {
+                default = '5m',
+              }
+            },
+          })
+        end,
       },
       extensions = {
         mcphub = {
